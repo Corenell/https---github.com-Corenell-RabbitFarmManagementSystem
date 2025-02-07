8d6cfacde160dca9d46535867950cff7dc11dcd3
matrix <2096838633@qq.com>
2025-02-07 18:36:04 +0800

    更新为180度舵机代码

--- Sensor/Test.Servo_OUT.HX711/Test.Servo_OUT.HX711.ino
+++ Sensor/Test.Servo_OUT.HX711/Test.Servo_OUT.HX711.ino
@@ -3,7 +3,7 @@
 #include "soc/rtc.h"
 #include <ESP32Servo.h>
 
-Servo servo;
+Servo servo1;
 
 #define CALIBRATION_FACTOR 695 //校准因子
 #define WEIGHT 50 //定量重量
@@ -19,7 +19,7 @@ void setup() {
   Serial.println("正在初始化");
   setCpuFrequencyMhz(80); 
   Serial.println("HX711");
-  servo.attach(14);
+  servo1.attach(14);
   Serial.println("Servo_OUT");
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale.set_scale(CALIBRATION_FACTOR); //该值是通过用已知重量校准天平而获得的
@@ -35,24 +35,13 @@ void loop() {
     if(weight >= WEIGHT){
       Serial.print("达到目标值:");
       Serial.println(weight);
-      
-      servo.writeMicroseconds(1000);
-      delay(2000);//顺时针120度
-
-      servo.writeMicroseconds(1500);
-      delay(1000);//停1s
-
-       servo.writeMicroseconds(2000);
-       delay(1930);//逆时针120度
-       //360舵机不好精确控制角度有一个误差，暂时这么用把，等180度舵机来了换
-
-      servo.writeMicroseconds(1500);//停止
-  
-      
+      servo1.write(1);
+      delay(2000);
+      servo1.write(120);
     }
   }
   
-  //Serial.println(weight);
+  Serial.println(weight);
   delay(50);
   
 }
