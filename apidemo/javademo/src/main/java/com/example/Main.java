7c43c8370e31f8279b35221afe01e5a98a8442ef
王晨宇 <wang_chen_yu@outlook.com>
2025-02-23 02:34:37 +0800

    demo

---
+++ apidemo/javademo/src/main/java/com/example/Main.java
@@ -0,0 +1,166 @@
+// 所需的okhttp库
+package com.example;
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        try {
+            // 调试输出信息
+            String token = fetchAuthToken();
+            System.out.println("token: " + token);
+            String response = getshuxing(token);
+            System.out.println("response: " + response);
+            int status = postmessage(token);
+            System.out.println("status: " + status);
+
+
+        } catch (IOException e) {
+            System.err.println("请求发生错误: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    // post发起鉴权请求并返回响应头中的鉴权 Token 数据
+    public static String fetchAuthToken() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        
+        // 请求鉴权的 URL
+        String url = "https://iam.cn-north-4.myhuaweicloud.com/v3/auth/tokens";
+        
+        // 构造 JSON 格式的请求数据
+        String jsonPayload = "{\n" +
+        "  \"auth\": {\n" +
+        "    \"identity\": {\n" +
+        "      \"methods\": [\"password\"],\n" +
+        "      \"password\": {\n" +
+        "        \"user\": {\n" +
+        "          \"name\": \"qing_lan\",\n" +
+        "          \"password\": \"cy383245\",\n" +
+        "          \"domain\": {\n" +
+        "            \"name\": \"qing_lan\"\n" +
+        "          }\n" +
+        "        }\n" +
+        "      }\n" +
+        "    },\n" +
+        "    \"scope\": {\n" +
+        "      \"project\": {\n" +
+        "        \"name\": \"cn-north-4\"\n" +
+        "      }\n" +
+        "    }\n" +
+        "  }\n" +
+        "}";
+    
+        MediaType JSON_TYPE = MediaType.get("application/json; charset=utf-8");
+        RequestBody body = RequestBody.create(jsonPayload, JSON_TYPE);
+        
+        // 构造 POST 请求
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .addHeader("Content-Type", "application/json") 
+                .build();
+
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+            
+            if (!response.isSuccessful()) {
+                throw new IOException("请求失败: " + response.code());
+            }
+            
+            // 从响应头中提取鉴权 Token 数据
+            String token = response.header("X-Subject-Token");
+            return token;
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+    }
+
+
+    // get获取设备属性返回响应体中的json数据
+    public static String getshuxing(String token) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        
+        // 请求 URL
+        String url = "https://ef861ca468.st1.iotda-app.cn-north-4.myhuaweicloud.com:443/v5/iot/b3318d2e70ab4740b267ed8955fd7275/devices/67b683d83f28ab3d0384f27e_rabbit/properties?service_id=67b683d83f28ab3d0384f27e_rabbit";
+        
+        
+        // 构造 get 请求
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("Content-Type", "application/json")
+                .addHeader("X-Auth-Token", token)  // 这里传递 fetchAuthToken() 方法获取的 token
+                .build();
+
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+            
+            if (!response.isSuccessful()) {
+                throw new IOException("请求失败: " + response.code());
+            }
+            
+            // 提取返回数据
+            String message = response.body().string();
+            return message;
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+    }
+
+    // post消息返回状态码
+    public static int postmessage(String token) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        
+        // post URL
+        String url = "https://ef861ca468.st1.iotda-app.cn-north-4.myhuaweicloud.com:443/v5/iot/b3318d2e70ab4740b267ed8955fd7275/devices/67b683d83f28ab3d0384f27e_rabbit/messages";
+        
+        // 构造 JSON 格式的请求数据
+        String jsonPayload = "{\n"
+        + "  \"message\": {\n"
+        + "    \"pwm\": 50,\n"
+        + "    \"num\": 1\n"
+        + "  }\n"
+        + "}";
+    
+        MediaType JSON_TYPE = MediaType.get("application/json; charset=utf-8");
+        RequestBody body = RequestBody.create(jsonPayload, JSON_TYPE);
+        
+        // 构造 POST 请求
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .addHeader("Content-Type", "application/json") 
+                .addHeader("X-Auth-Token", token)  // 这里传递 fetchAuthToken() 方法获取的 token
+                .build();
+
+        Response response = null;
+        try {
+            response = client.newCall(request).execute();
+            
+            if (!response.isSuccessful()) {
+                throw new IOException("请求失败: " + response.code());
+            }
+            int status = response.code();
+            return status;
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+    }
+}
+
+
--- apidemo/pythondemo/pythondemo.py
+++ apidemo/pythondemo/pythondemo.py
@@ -1,17 +1,17 @@
-import requests # http请求库
-import json # json库
+# 导入库
+import requests
 
-# post请求进行身份认证，获取token，24小时内可用
-def iot_authentication():
-    # 请求鉴权连接地址
+# 发起鉴权请求并返回响应头中的鉴权 Token 数据
+def fetch_auth_token():
+    # 请求鉴权的 URL
     url = "https://iam.cn-north-4.myhuaweicloud.com/v3/auth/tokens"
-    # 云端的身份验证信息，需要填写的地方有4个，数据结构是json
-    payload = json.dumps({
+    headers = {"Content-Type": "application/json"}
+
+    # 构造 JSON 格式的请求数据
+    json_payload = {
         "auth": {
             "identity": {
-                "methods": [
-                    "password"
-                ],
+                "methods": ["password"],
                 "password": {
                     "user": {
                         "name": "qing_lan",
@@ -28,36 +28,72 @@ def iot_authentication():
                 }
             }
         }
-    })
-    headers = {
-        'Content-Type': 'application/json'
     }
-    # 发起网络请求获取华为云鉴权，post请求，
-    response = requests.post(url, headers=headers, data=payload)
-    # json方式提取到的json信息，是必要的鉴权信息
-    token = response.headers["X-Subject-Token"]
+
+    # 构造 POST 请求
+    response = requests.post(url, json=json_payload, headers=headers)
+    if response.status_code != 201:
+        raise Exception(f"请求失败: {response.status_code} {response.text}")
+
+    # 从响应头中提取鉴权 Token 数据
+    token = response.headers.get("X-Subject-Token")
     return token
 
-
-#发送一个get请求到云端，云端返回json数据
-def iot_shadow_data(token):
-    # 请求url，网址是获取设备影子数据
-    url = "https://ef861ca468.st1.iotda-app.cn-north-4.myhuaweicloud.com:443/v5/iot/b3318d2e70ab4740b267ed8955fd7275/devices/67b683d83f28ab3d0384f27e_rabbit/shadow"
-    # headers需要添加请求的鉴权信息
+# 获取设备属性并返回响应体中的 JSON 数据
+def get_device_properties(token):
+    # 请求 URL
+    url = "https://ef861ca468.st1.iotda-app.cn-north-4.myhuaweicloud.com:443/v5/iot/b3318d2e70ab4740b267ed8955fd7275/devices/67b683d83f28ab3d0384f27e_rabbit/properties?service_id=environment"
     headers = {
-            "Content-Type": "application/json",
-            "X-Auth-Token": token
+        "Content-Type": "application/json",
+        "X-Auth-Token": token  # 这里传递 fetch_auth_token() 方法获取的 token
+    }
+
+    # 构造 GET 请求
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f"请求失败: {response.status_code} {response.text}")
+
+    # 提取返回数据
+    return response.json()
+
+# 发送消息并返回状态码
+def post_message(token):
+    # 请求 URL
+    url = "https://ef861ca468.st1.iotda-app.cn-north-4.myhuaweicloud.com:443/v5/iot/b3318d2e70ab4740b267ed8955fd7275/devices/67b683d83f28ab3d0384f27e_rabbit/messages"
+    headers = {
+        "Content-Type": "application/json",
+        "X-Auth-Token": token  # 这里传递 fetch_auth_token() 方法获取的 token
+    }
+
+    # 构造 JSON 格式的请求数据
+    json_payload = {
+        "message": {
+            "pwm": 50,
+            "num": 1
         }
-    response = requests.request("GET", url, headers=headers)
-    # 转化为json数据
-    data = response.json()
-    return data
+    }
 
+    # 构造 POST 请求
+    response = requests.post(url, json=json_payload, headers=headers)
+    status = response.status_code
+    if response.status_code != 200 and response.status_code != 201:
+        raise Exception(f"请求失败: {response.status_code} {response.text}")
 
+    return status
 
-    
-# 获取身份验证token
-token = iot_authentication()
-#获取设备影子数据
-data = iot_shadow_data(token)
-print(data)
+def main():
+    try:
+        token = fetch_auth_token()
+        print("Token:", token)
+
+        response = get_device_properties(token)
+        print("Response:", response)
+
+        status = post_message(token)
+        print("Status:", status)
+
+    except Exception as e:
+        print("请求发生错误:", e)
+
+if __name__ == "__main__":
+    main()
