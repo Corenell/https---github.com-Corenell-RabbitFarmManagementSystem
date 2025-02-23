17967790a5614d77c0e47e0c960e44ea12263e14
王晨宇 <wang_chen_yu@outlook.com>
2025-02-23 17:36:14 +0800

    iot改后代码

---
+++ iot_sensor/environment.ino
@@ -0,0 +1,222 @@
+#include <Adafruit_AHTX0.h>// 引用库
+#include <BMP280.h>
+#include <WiFi.h>         // 用于连接 WiFi
+#include <PubSubClient.h> // 用于 MQTT 连接和通信
+#include <ArduinoJson.h>  // 用于构造 JSON 报文
+
+
+#define MQ135D 25       //定义光敏传感器数字输出引脚
+#define MQ135A 34       //定义光敏传感器模拟输出引脚
+
+
+//I2C接线// 引脚定义
+const int FAN_PIN = 13;  // 风扇连接的 PWM 引脚
+const int PUMP_PIN = 12;  // 风扇连接的 PWM 引脚
+
+// PWM 参数
+const int PWM_FREQ = 1000;  // PWM 频率（Hz）
+const int PWM_RESOLUTION = 8;  // PWM 分辨率（8 位，范围为 0-255）
+//const int PWM_CHANNEL = 0;  // PWM 通道（ESP32 有 16 个通道，0-15）
+
+const int PWM_FREQ2 = 1000;  // PWM 频率（Hz）
+const int PWM_RESOLUTION2 = 8;  // PWM 分辨率（8 位，范围为 0-255）
+//const int PWM_CHANNEL2 = 1;  // PWM 通道（ESP32 有 16 个通道，0-15）
+//SDA 21  SCL 22
+
+// WiFi 和 MQTT 连接信息
+const char* ssid = "Creator_Space";
+const char* password = "iloveSCU";
+const char* mqttServer = "ef861ca468.st1.iotda-device.cn-north-4.myhuaweicloud.com";
+const int mqttPort = 1883;
+const char* clientId = "67b683d83f28ab3d0384f27e_environment_0_0_2025022306";
+const char* mqttUser = "67b683d83f28ab3d0384f27e_environment";
+const char* mqttPassword = "17a85498dc8339943237186c61e7aa2861be33405971bd0eab52d080f762ae92";
+//mqtt连接信息
+String half_get_properties = "$oc/devices/67b683d83f28ab3d0384f27e_environment/sys/properties/get/request_id=";
+String half_response_properties = "$oc/devices/67b683d83f28ab3d0384f27e_environment/sys/properties/get/response/request_id=";
+String get_messages = "$oc/devices/67b683d83f28ab3d0384f27e_environment/sys/messages/down";
+
+
+BMP280 bmp280;
+Adafruit_AHTX0 aht;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void MQTT_Init() {
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(3000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to the WiFi network");
+  client.setServer(mqttServer, mqttPort);
+  client.setKeepAlive(60);
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect(clientId, mqttUser, mqttPassword)) {
+      Serial.println("Connected to MQTT");
+    } else {
+      Serial.print("Failed to connect, state: ");
+      Serial.println(client.state());
+      delay(6000);
+    }
+  }
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  client.setCallback(callback);
+
+
+}
+
+
+
+void callback(char* topic, byte* message, unsigned int length) {
+
+  Serial.print("topic: ");
+  Serial.println(topic);
+  String topicStr = String(topic);
+
+  String receivedMessage = ""; 
+  for (int i = 0; i < length; i++) {
+    receivedMessage += (char)message[i];
+  }
+  Serial.print("message: ");
+  Serial.println(receivedMessage);
+  
+  if (topicStr.startsWith(half_get_properties)) {
+    String requestId = topicStr.substring(half_get_properties.length());
+    MQTT_response1(requestId);  // 调用 MQTT_response1
+}
+  else if (topicStr == get_messages) {
+    MQTT_response2(receivedMessage);  // 调用 MQTT_response2
+  }
+  else {
+    // 如果 topic 不是指定的 topic
+    Serial.println("Unknown topic");
+  }
+}
+
+
+
+void MQTT_response1(String requestId) {
+
+  //uint32_t pressure = bmp280.getPressure();  //BMP280填充气压
+  sensors_event_t humidity, temp;  //AHT20填充温湿度
+  aht.getEvent(&humidity, &temp);
+  float tempread = temp.temperature;
+  float humiread = humidity.relative_humidity;
+  float NH3Value = analogRead(MQ135A);
+  float weight = 0.12;
+  //调试输出
+  Serial.print("温度: "); Serial.print(tempread); Serial.println("℃");
+  Serial.print("湿度: "); Serial.print(humiread); Serial.println("%");
+  //Serial.print("气压: "); Serial.print(pressure/1000); Serial.println("KPa");
+  Serial.println(NH3Value);//串口打印模拟信号0-4095
+
+
+  // 构造 JSON 响应
+  JsonDocument responseDoc;
+  responseDoc["temperature"] = tempread;
+  responseDoc["humidity"] = humiread;
+  responseDoc["ammonia"] = NH3Value;
+  responseDoc["weight"] = weight;
+  // 序列化响应消息
+  String responseMessage;
+  serializeJson(responseDoc, responseMessage);
+  // 更新响应的主题，包含 request_id
+
+
+  String responseTopic = half_response_properties + requestId;
+  if (client.publish(responseTopic.c_str(), responseMessage.c_str())) {
+    Serial.println("Response sent success");
+  } else {
+    Serial.println("Error sending response");
+  }
+}
+
+
+
+void MQTT_response2(String receivedMessage) {
+ JsonDocument doc;
+  DeserializationError error = deserializeJson(doc, receivedMessage);
+  if (error) {
+    Serial.print("Failed to parse JSON: ");
+    Serial.println(error.f_str());
+    return;
+  }
+  // 提取 fan_power 和 water_curtain_power
+  int fanPower = doc["content"]["fan_power"];
+  int waterCurtainPower = doc["content"]["water_curtain_power"];
+
+
+  fanPower = constrain(fanPower, 0, 100);
+  waterCurtainPower = constrain(waterCurtainPower, 0, 100);
+  int power = round(fanPower * 255.0 / 100.0);
+  int power2 = round(waterCurtainPower * 255.0 / 100.0);
+
+  ledcWrite(FAN_PIN, power);  // 设置 PWM 占空比
+  ledcWrite(PUMP_PIN, power2);  // 设置 PWM 占空比
+
+  // 输出结果
+  Serial.print("Fan Power: ");
+  Serial.println(fanPower);
+  Serial.print("Water Curtain Power: ");
+  Serial.println(waterCurtainPower);
+}
+
+// MQTT 重新连接函数
+void reconnect() {
+  int attempt = 0;
+  while (!client.connected()) {
+    Serial.printf("Attempt %d: Reconnecting to MQTT...\n", ++attempt);
+    if (client.connect(clientId, mqttUser, mqttPassword)) {
+      Serial.println("Reconnected to MQTT");
+    } else {
+      Serial.printf("Failed to reconnect, state: %d. Retrying...\n", client.state());
+      delay(6000);
+    }
+    // 添加超时机制
+    if (attempt > 30) {
+      Serial.println("Reconnect timeout, restarting...");
+      ESP.restart();
+    }
+  }
+}
+
+
+
+void setup() {
+  Serial.begin(115200);//初始化串口
+  Serial.println("AHT20+MQ135");
+  MQTT_Init();
+  pinMode(MQ135D, INPUT);//定义GPIO15为输入模式
+  pinMode(MQ135A, INPUT);//定义GPIO34为输入模式
+  Wire.begin(); 
+
+  bmp280.begin();  //初始化BMP280
+
+  ledcAttach(FAN_PIN, PWM_FREQ, PWM_RESOLUTION); 
+  ledcAttach(PUMP_PIN, PWM_FREQ2, PWM_RESOLUTION2); 
+  //Aht20初始化
+  while(!aht.begin()){
+    Serial.println("Aht20 initial error!");
+    delay(500);
+  }
+  Serial.println("AHT20 found");
+
+}
+
+
+
+
+void loop() {
+
+    if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  delay(1000);
+}
---
+++ iot_sensor/feed.ino
@@ -0,0 +1,320 @@
+#include <ESP32Servo.h>
+#include "HX711.h"
+#include "soc/rtc.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+// 硬件引脚定义
+#define LOADCELL_DOUT_PIN 17
+#define LOADCELL_SCK_PIN  16
+
+#define CALIBRATION_FACTOR 719 //校准因子
+
+
+// WiFi 和 MQTT 连接信息
+const char* ssid = "Creator_Space";
+const char* password = "iloveSCU";
+const char* mqttServer = "ef861ca468.st1.iotda-device.cn-north-4.myhuaweicloud.com";
+const int mqttPort = 1883;
+const char* clientId = "67b683d83f28ab3d0384f27e_feedcar_0_0_2025022308";
+const char* mqttUser = "67b683d83f28ab3d0384f27e_feedcar";
+const char* mqttPassword = "98c142b691bea4b4265fc308fcea02d847d0d2d8b667b7ba1d18caadc834a825";
+
+String half_get_properties = "$oc/devices/67b683d83f28ab3d0384f27e_feedcar/sys/properties/get/request_id=";
+String half_response_properties = "$oc/devices/67b683d83f28ab3d0384f27e_feedcar/sys/properties/get/response/request_id=";
+String get_messages = "$oc/devices/67b683d83f28ab3d0384f27e_feedcar/sys/messages/down";
+
+
+// 系统参数
+float TARGET_WEIGHT = 1;  // 目标投喂量（克）
+float TARGET_NUM = 1;  //云端发送的饲料参数
+int State = 0;  //喂料初始状态为0（不执行）
+const int FEEDING_SPEED = 2000;     // 输料舵机全速运转角度 1500-2000 顺
+const int STOP_ANGLE = 1500;         // 输料舵机停止角度
+const int DUMP_ANGLE = 1;        // 翻斗倾倒角度
+const int DUMP_ANGLE2 = 30;       //翻斗倾倒角度2
+const int RETURN_ANGLE = 120;        // 翻斗复位角度
+const int SETTLE_TIME = 10000;      // 料斗稳定时间(ms)
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+void MQTT_Init() {
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(3000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to the WiFi network");
+  client.setServer(mqttServer, mqttPort);
+  client.setKeepAlive(60);
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect(clientId, mqttUser, mqttPassword)) {
+      Serial.println("Connected to MQTT");
+    } else {
+      Serial.print("Failed to connect, state: ");
+      Serial.println(client.state());
+      delay(6000);
+    }
+  }
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  client.setCallback(callback);
+
+
+}
+
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.println(topic);
+  String topicStr = String(topic);
+  String receivedMessage = ""; 
+  for (int i = 0; i < length; i++) {
+    receivedMessage += (char)message[i];
+  }
+  Serial.print("Message: ");
+  Serial.println(receivedMessage);
+  
+  if (topicStr == get_messages) {
+    String requestId = topicStr.substring(half_get_properties.length());
+    MQTT_response1(receivedMessage); 
+  }
+}
+
+
+
+// 接受预期投喂量，开始投喂饲料
+void MQTT_response1(String receivedMessage) {
+ JsonDocument doc;
+  DeserializationError error = deserializeJson(doc, receivedMessage);
+  if (error) {
+    Serial.print("Failed to parse JSON: ");
+    Serial.println(error.f_str());
+    return;
+  }
+  // 解析json数据
+  int weight = doc["a"]["a"];
+  // 如果满足一定条件，开始投喂直到一次投喂完成执行发送投喂完成函数
+  // MQTT_response2(String receivedMessage)
+
+
+}
+
+
+
+// // 投喂完成函数，给esp32小车发送开始投喂指令
+// void MQTT_response2(String receivedMessage) {
+
+//   // 构造 JSON 响应
+//   JsonDocument responseDoc;
+//   responseDoc["feed"] = 1;
+
+//   // 序列化响应消息
+//   String responseMessage;
+//   serializeJson(responseDoc, responseMessage);
+//   // 更新响应的主题，包含 request_id
+
+
+//   String responseTopic = post_properties
+//   if (client.publish(responseTopic.c_str(), responseMessage.c_str())) {
+//     Serial.println("Response sent success");
+//   } else {
+//     Serial.println("Error sending response");
+//   }
+// }
+
+
+
+
+
+
+void reconnect() {
+  int attempt = 0;
+  while (!client.connected()) {
+    Serial.printf("Attempt %d: Reconnecting to MQTT...\n", ++attempt);
+    if (client.connect(clientId, mqttUser, mqttPassword)) {
+      Serial.println("Reconnected to MQTT");
+    } else {
+      Serial.printf("Failed to reconnect, state: %d. Retrying...\n", client.state());
+      delay(6000);
+    }
+  }
+}
+
+
+
+
+// 系统状态
+enum SystemState { IDLE, FEEDING, DUMPING };
+SystemState currentState = IDLE;  //初始状态为闲置
+unsigned long actionStartTime = 0;
+
+Servo servo360;  // 输料舵机（360°连续旋转）
+Servo servo180;  // 翻斗舵机（180°标准）
+HX711 scale;  //HX711
+
+// 移动平均滤波（称重稳定性较好，暂时关闭滤波）
+float filteredWeight() {
+  /*
+  static float buffer[5];
+  static byte index = 0;
+  
+  buffer[index] = scale.get_units();
+  index = (index + 1) % 5;
+  
+  float sum = 0;
+  for(byte i=0; i<5; i++) sum += buffer[i];
+  return sum / 5;
+  */
+  float num = scale.get_units();
+  return num;
+}
+
+void setup() {
+  Serial.begin(115200);
+  MQTT_Init();
+  Serial.println("正在初始化");
+  setCpuFrequencyMhz(80);  //HX711需要esp32工作在80MHz
+  
+  // 初始化舵机
+  servo360.attach(25);  // 输料螺杆舵机（连续旋转）
+  servo180.attach(26);  // 翻斗舵机
+  servo360.write(STOP_ANGLE);  // 初始停止状态1500
+  servo180.write(RETURN_ANGLE);  //初始水平状态120
+  Serial.println("Servo Ready!");
+
+  // 初始化称重模块
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(CALIBRATION_FACTOR);  //设置校准因子
+  scale.tare();  //称 清零
+  Serial.println("HX711 Ready!");
+
+}
+
+
+void loop() {
+    if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  delay(1000);
+}
+  // float currentWeight = filteredWeight();  //获取当前料斗上的饲料重量
+
+  // //从IOT获取State值
+  // if(State == 1){  //State为1，开始投喂流程
+
+  // switch(currentState) {
+  //   case IDLE:  //闲置
+  //     TARGET_WEIGHT = TARGET_NUM;  //获取新的目标投喂量
+  //     if(currentWeight <= TARGET_WEIGHT) {  //是否达到目标量
+  //       currentState = FEEDING;  //切换为FEEDING状态
+  //       servo360.writeMicroseconds(FEEDING_SPEED);  // 启动输料
+  //       Serial.println("开始输料...");
+  //     }
+  //     break;
+
+  //   case FEEDING:  //喂料
+  //     if(currentWeight >= TARGET_WEIGHT) {  //是否达到目标量
+  //       servo360.writeMicroseconds(STOP_ANGLE);  // 停止输料
+  //       currentState = DUMPING;  //切换为倒料状态
+  //       actionStartTime = millis();     // 记录饲料称重完成的时刻
+  //       Serial.println("达到目标重量，准备倾倒");
+  //     }
+  //     break;
+
+  //   case DUMPING: {  //倾倒
+  //     static uint8_t phase = 0;  //初始状态0
+  //     unsigned long elapsed = millis() - actionStartTime;  //计算动作时刻到目前的时间差
+
+  //     switch(phase) {
+  //       case 0: //执行倾倒
+  //         if(elapsed > SETTLE_TIME) {  //时间差大于稳定间隔
+  //           servo180.write(DUMP_ANGLE);  //倾倒
+  //           actionStartTime = millis();  //更新倾倒时刻
+  //           phase = 1;  
+  //         }
+  //         break;
+          
+  //       case 1: // 抖动倾倒
+  //         if(elapsed > 1000) {  //时间差大于稳定间隔
+  //           servo180.write(DUMP_ANGLE2);  //90度位
+  //           actionStartTime = millis();  //更新抖动时刻
+  //           phase = 2;
+  //         }
+  //         break;
+          
+  //       case 2: // 再次倾倒
+  //         if(elapsed > 1000) {  //时间差大于稳定间隔
+  //           servo180.write(DUMP_ANGLE);  //再次倾倒
+  //           actionStartTime = millis();  //更新倾倒时刻
+  //           phase = 3;
+  //         }
+  //         break;
+
+  //       case 3: // 保持倾倒
+  //         if(elapsed > 3000) {  //时间差大于稳定间隔
+  //           servo180.write(RETURN_ANGLE); //料斗复位
+  //           actionStartTime = millis();  //更新动作时间
+  //           phase = 4;
+  //         }
+  //         break;
+  //       case 4: // 复位完成
+  //         if(elapsed > 2000) {  //时间差大于稳定间隔
+  //           scale.tare();  //称 清零
+  //           currentState = IDLE;  //切换为闲置状态
+  //           phase = 0;  //切换状态0
+  //           Serial.println("完成投喂循环");
+  //           State = 0;  //完成投喂
+  //         }
+  //         break;
+  //     }
+  //     break;
+  //   }
+  // }
+  // }
+
+  // //向IOT更新当前State
+  
+  // // 调试输出
+  // static unsigned long lastPrint = 0;  //上次输出时刻
+  // if(millis() - lastPrint > 1000) {  //间隔1s
+  //   Serial.print("当前状态: ");
+  //   Serial.print(State);
+  //   Serial.print(" | 流程: ");
+  //   Serial.print(currentState);
+  //   Serial.print(" | 重量: ");
+  //   Serial.print(currentWeight);
+  //   Serial.println("g");
+  //   Serial.print("目标重量: ");
+  //   Serial.print(TARGET_WEIGHT);
+  //   Serial.println("g");
+  //   lastPrint = millis();  //更新输出时刻
+  // }
+  
+  // //delay(50);  // 主循环延迟
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
