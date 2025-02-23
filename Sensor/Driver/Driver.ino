cfbd31f5724e6439218194df5ba3bd1fe8171805
matrix <matrix-ze@qq.com>
2025-02-23 15:12:49 +0800

    更新驱动测试代码

--- Sensor/Driver/Driver.ino
+++ Sensor/Driver/Driver.ino
@@ -1,28 +1,30 @@
 // 引脚定义
-const int PUL_PIN = 5;  // 脉冲信号引脚（PUL）
-const int DIR_PIN = 6;  // 方向信号引脚（DIR）
+const int PUL_PIN = 16;  // 脉冲信号引脚（PUL）
+const int DIR_PIN = 17;  // 方向信号引脚（DIR）
+const int EN_PIN = 4;    //使能引脚
 
 // 参数定义
-const long PULSE_COUNT = 110000;  // 脉冲数量（控制电机旋转角度）
-const int PULSE_DELAY = 26;       // 脉冲周期（微秒，控制电机转速）
-const int DELAY_BETWEEN_MOVES = 1000;  // 正反转之间的延时（毫秒）
+const long PULSE_COUNT = 500;  // 脉冲数量（控制电机旋转角度）
+const int PULSE_DELAY = 26;    // 脉冲周期（微秒，控制电机转速）
 
 void setup() {
   // 初始化引脚
   pinMode(PUL_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
+  digitalWrite(EN_PIN,LOW);
 }
 
 void loop() {
   // 正转
   digitalWrite(DIR_PIN, HIGH);  // 设置方向为正转
   generatePulses(PULSE_COUNT, PULSE_DELAY);  // 生成脉冲
-  delay(DELAY_BETWEEN_MOVES);  // 延时
-
+  delay(5000);  // 延时
+/*
   // 反转
   digitalWrite(DIR_PIN, LOW);  // 设置方向为反转
   generatePulses(PULSE_COUNT, PULSE_DELAY);  // 生成脉冲
   delay(DELAY_BETWEEN_MOVES);  // 延时
+  */
 }
 
 // 生成指定数量的脉冲
